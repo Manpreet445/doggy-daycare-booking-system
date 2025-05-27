c821fc365839c32c9b072e44b393cd881e8e0f82
Manpreet445 <ManpreetSingh3@edu.sait.ca>
2025-05-26 20:33:27 -0600

    Update booking.js

--- scripts/booking.js
+++ scripts/booking.js
@@ -1,7 +1,5 @@
 /********* create variables *********/
-// useful variables might be: the cost per day, the number of days selected, and elements on the screen that will be clicked or will need to be modified. 
-// Do any of these variables need to be initialized when the page is loaded? 
-// When do they need to be reset or updated?
+
 let costPerDay = 35; // Initial cost is for a full day
 let numberOfDaysSelected = 0;
 let dayButtons = document.querySelectorAll('.day-selector li');
@@ -12,10 +10,8 @@ let calculatedCostElement = document.getElementById('calculated-cost');
 let clickedClass = 'clicked';
 
 /********* colour change days of week *********/
-// when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
-// added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
-// function handleDayClick() {
-    
+
+
 
 function clickHandlerFor(button) {
   return function handleClick() {
@@ -43,7 +39,7 @@ function setupAllDayButtons() {
 // Initialize everything
 setupAllDayButtons();
 /********* clear days *********/
-// when the clear-button is clicked, the "clicked" class is removed from all days, any other relevant variables are reset, and the calculated cost is set to 0.
+
 
 function clearAllDays() {
     for (let i = 0; i < 5; i++) {
@@ -59,7 +55,7 @@ function clearAllDays() {
 
 
 /********* change rate *********/
-// when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
+
 function selectHalfDay() {
     costPerDay = 20;
     halfDayButton.classList.add(clickedClass);
@@ -69,7 +65,7 @@ function selectHalfDay() {
 
   halfDayButton.addEventListener('click', selectHalfDay);
 
-// when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
+
 function selectFullDay() {
     costPerDay = 35;
     fullDayButton.classList.add(clickedClass);
@@ -85,7 +81,7 @@ fullDayButton.addEventListener('click', selectFullDay);
 
 
 /********* calculate *********/
-// when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
+
 function calculateTotalCost() {
     var totalCost = numberOfDaysSelected * costPerDay;
     calculatedCostElement.innerHTML = totalCost;
